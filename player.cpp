07281f0ff242559c5d004d914e4d04ce4e04a874
Eric Vacca <evacca@newfieldwireless.com>
2016-07-21 11:46:41 -0700

    fix crash

--- player.cpp
+++ player.cpp
@@ -1,12 +1,9 @@
 #include "player.h"
 #include "json.h"
-#include <iostream>
-
 const char* Player::VERSION = "Default C++ folding player";
 
 int Player::betRequest(json::Value game_state)
 {
-    std::err << json::Serialize(game_state);
     return 100;
 }
 
