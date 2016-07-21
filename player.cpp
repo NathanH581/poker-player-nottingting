92bb43870fc70eef0d1f8e9ee6e526ad3fbb0375
starry225 <tingtingma0225@gmail.com>
2016-07-21 12:18:33 -0700

    print token

--- player.cpp
+++ player.cpp
@@ -14,8 +14,7 @@ int Player::betRequest(json::Value game_state)
     size_t pos = 0;
     while ((pos = s.find(delimiter)) != std::string::npos) {
         token = s.substr(0,pos);
-        std::cout << token << std::endl;
-        std::cout << std::endl;
+        std::cerr << token << std::endl;
         s.erase(0, pos + delimiter.length());
     }
 
