7620ec60a4471a05a50b1e398a6a5fee6ff15e42
starry225 <tingtingma0225@gmail.com>
2016-07-21 12:42:55 -0700

    add try catch

--- player.cpp
+++ player.cpp
@@ -7,18 +7,26 @@ const char* Player::VERSION = "Default C++ player";
 
 int Player::betRequest(json::Value game_state)
 {
-    std::cerr<<json::Serialize(game_state)<<std::endl;
-    std::string s = json::Serialize(game_state);
-    std::string delimiter = "},{";
-    std::string token;
-    size_t pos = 0;
-    while ((pos = s.find(delimiter)) != std::string::npos) {
-        token = s.substr(0,pos);
-        std::cerr << token << std::endl;
-        s.erase(0, pos + delimiter.length());
-    }
+    
+    try {
+        std::cerr<<json::Serialize(game_state)<<std::endl;
+        std::string s = json::Serialize(game_state);
+        std::string delimiter = "},{";
+        std::string token;
+        size_t pos = 0;
+        while ((pos = s.find(delimiter)) != std::string::npos) {
+            token = s.substr(0,pos);
+            std::cerr << token << std::endl;
+            s.erase(0, pos + delimiter.length());
+        }
 
-    return 100;
+        return 100;
+        
+        
+    } catch(const std::exception& e) {
+        // in case it crashes
+        return 100;
+    }
 
 }
 
