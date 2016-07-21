c23fed92b723e21b37ab576d83f09513d6945600
Eric Vacca <evacca@newfieldwireless.com>
2016-07-21 11:38:39 -0700

    first

--- player.cpp
+++ player.cpp
@@ -1,10 +1,13 @@
 #include "player.h"
+#include "json.h"
+#include <iostream>
 
 const char* Player::VERSION = "Default C++ folding player";
 
 int Player::betRequest(json::Value game_state)
 {
-    return 0;
+    std::err << json::Serialize(game_state);
+    return 100;
 }
 
 void Player::showdown(json::Value game_state)
