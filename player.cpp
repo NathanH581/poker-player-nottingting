1eb269b354cc996bcbe55016dd763775589da896
lyang7Netscout <huiyingyang@hotmail.com>
2016-07-21 12:06:48 -0700

    parse log

--- player.cpp
+++ player.cpp
@@ -5,8 +5,18 @@ const char* Player::VERSION = "Default C++ folding player";
 
 int Player::betRequest(json::Value game_state)
 {
-    std::cerr<<json::Serialize(game_state);
+    std::cerr<<json::Serialize(game_state)<<std::endl;
+    std::string s = json::Serialize(game_state);
+    std::string delimiter = "{";
+    std::string token;
+    size_t pos = 0;
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0,pos);
+        std::cout << token << std::endl;
+        s.erase(0, pos + delimiter.length());
+    }
     return 100;
+
 }
 
 void Player::showdown(json::Value game_state)
