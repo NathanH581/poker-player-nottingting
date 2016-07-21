eeba3659ac8c80ce91bbf2ab5e3a6db54782e525
starry225 <tingtingma0225@gmail.com>
2016-07-21 13:48:09 -0700

    get hole cards

--- player.cpp
+++ player.cpp
@@ -1,12 +1,20 @@
 #include "player.h"
 #include "json.h"
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 const char* Player::VERSION = "Default C++ player";
 
 int Player::betRequest(json::Value game_state)
 {
     try {
+        std::map<string, vector<string> > map_holecards;
+	    //map<string, vector<string> > map_community;
+	
         std::cerr<<json::Serialize(game_state)<<std::endl;
         std::string s = json::Serialize(game_state);
         std::string delimiter = "},{";
@@ -37,8 +45,31 @@ int Player::betRequest(json::Value game_state)
                 if (player.HasKey("stack")) {
                     int currentChipCount = player["stack"].ToInt();
                 }
+                
+                // get hole cards
+                for ( auto iter_holecards = hole_cards.begin(); iter_holecards != hole_cards.end(); iter_holecards++ ) {
+		            string suit = (*iterator)["suit"].ToString();
+		            string rank = (*iterator)["rank"].ToString();
+		            if ( map_holecards.find(suit) != map_holecards.end() ) {
+			            vector<string> vec;
+		    	        vec.push_back(rank);
+			            map_holecards.insert(make_pair(suit, vec));
+		            }
+		            else {
+			            map_holecards[suit].push_back(rank);
+		            }
+		        }
+                // end get hole cards
             }
         }
+        // print our hole cards
+        std::cerr << "see our cards ======> " << std::endl;
+	    for (auto it = map_holecards.begin(); it != map_holecards.end() ;it++) {
+	        std::cerr << "suit: " <<  it->first << std::endl;
+	        for ( auto it_vec = it->second.begin(); it_vec != it->second.end(); it_vec++) {
+		    cerr << "cards: " << *it_vec;
+	        }
+	    }// end print our hole cards
         
         json::Array community_cards = game_state["community_cards"].ToArray();
         std::cerr<<"we can see cards on table!!!!!!!!!!!!!!!!"<<std::endl;
